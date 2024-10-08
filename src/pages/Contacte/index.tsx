4c97055872374195f2acc1261bd967c176141360
Amorais19 <alicemoraisams@gmail.com>
2024-10-08 08:43:18 -0300

    cntacte

--- src/pages/Contacte/index.tsx
+++ src/pages/Contacte/index.tsx
@@ -3,7 +3,67 @@ import * as S from './styles'
 export function Contacte() {
   return (
     <S.Section>
-     
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Reem+Kufi:wght@400..700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap');
+      </style>
+      <section id='header'>
+        <img src="src/assets/teclado.png" alt="teclado" id='img' />
+        <div id="textos">
+          <h2 id='title'>Contacte-nos</h2>
+          <p id='text'>Não deixe de nos contactar caso haja problemas.
+            Procuramos estar cientes de erros para fornecermos segurança
+            e estabilidade em nossa plataforma.</p>
+        </div>
+      </section>
+      <section id='conteudo'>
+        <div id='contatos'>
+          <div id='grid'>
+            <div id="square">
+              <img src="" alt="" id='image' />
+              <p id='subtitle'>Phone</p>
+              <p id='subtext'>0800-0800</p>
+            </div>
+            <div id="square">
+              <img src="" alt="" id='image' />
+              <p id='subtitle'>Whatsapp</p>
+              <p id='subtext'>(35) 99999-0000</p>
+            </div>
+            <div id="square">
+              <img src="" alt="" id='image' />
+              <p id='subtitle'>E-mail</p>
+              <p id='subtext'>slasla@gmail.com</p>
+            </div>
+            <div id="square">
+              <img src="" alt="" id='image' />
+              <p id='subtitle'>Localização</p>
+              <p id='subtext'>Varginha-MG</p>
+            </div>
+          </div>
+          <div id="mapa">
+
+          </div>
+        </div>
+        <div id='suporte'>
+          <h3>SUPORTE</h3>
+          <div>
+            <p>Nome</p>
+            <input type="text" />
+            <label htmlFor=""></label>
+          </div>
+          <div>
+            <p>E-mail</p>
+            <input type="text" />
+            <label htmlFor=""></label>
+          </div>
+          <div>
+            <p>Mensagem</p>
+            <input type="text" />
+            <label htmlFor=""></label>
+          </div>
+          <button>Enviar</button>
+        </div>
+      </section>
     </S.Section>
   )
 }
--- src/pages/Contacte/styles.ts
+++ src/pages/Contacte/styles.ts
@@ -1,5 +1,112 @@
 import styled from 'styled-components'
+import { colors, gradientes } from '../../styles/GlobalStyle'
 
 export const Section = styled.section`
+    align-items: center;
+    justify-content: center;
 
+    #header {
+        display: inline-block;
+        position: relative;
+        width: 100%;
+        height: 30rem;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #img {
+        position: absolute;
+        width: 100%;
+        height: 30rem;
+    }
+
+    #textos {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #title {
+        font-family: 'Reem Kufi';
+        font-size: 4em;
+        color: ${colors.white};
+    }
+
+    #text {
+        width: 30%;
+        font-family: 'Raleway';
+        font-size: large;
+        color: ${colors.white};
+        text-align: center;
+        padding-top: 4rem;
+    }
+    
+    #conteudo {
+        display: grid;
+        grid-template-columns: auto auto;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+
+    #contatos {
+        padding: 2rem;
+    }
+
+    #grid {
+        display: grid;
+        grid-template-columns: auto auto;
+    }
+
+    #square {
+        margin: 1rem;
+        width: 10rem;
+        height: 10rem;
+        background-color: ${colors.roxoOpac};
+        border-radius: 10%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #subtitle {
+        font-family: 'Raleway';
+        color: ${colors.white};
+    }
+
+    #subtext {
+        font-family: 'Raleway';
+        font-size: smaller;
+        color: ${colors.white};
+        padding-top: 0.5rem;
+    }
+
+    #mapa {
+        width: 25rem;
+        height: 15rem;
+        border: 1px solid ${colors.roxoOpac};
+        margin-top: 2rem;
+    }
+
+    #suporte {
+        background-color: ${colors.blackFundo};
+        border-radius: 1.5%;
+        width: 100%;
+        height: 90%;
+        margin: 8rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+
+    h3 {
+        font-family: 'Reem Kufi';
+        font-size: xx-large;
+        color: ${colors.white};
+    }
 `
